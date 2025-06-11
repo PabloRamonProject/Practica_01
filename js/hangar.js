26eb7e2da6ee4e01b8ced26f13703d1f4d16c1ae
PabloRamonProject <pablo.r.bravo.kos@gmail.com>
2025-06-12 01:15:52 +0200

    Seleccion de naves

--- js/hangar.js
+++ js/hangar.js
@@ -34,4 +34,46 @@ $(document).ready(function() {
   $('.btn-faccion').on('mouseleave', function() {
     $('#preview-image').fadeOut(200);
   });
+
+  // Datos de las naves
+  const naves = [
+    {
+      nombre: 'Spectrum',
+      descripcion: 'Nave rápida y ágil, ideal para misiones de exploración.',
+      gif: 'img/nave2.gif'
+    },
+    {
+      nombre: 'Diminisher',
+      descripcion: 'Nave de combate balanceada, con buena resistencia y potencia.',
+      gif: 'img/nave1.gif'
+    },
+    {
+      nombre: 'Venom',
+      descripcion: 'Nave pesada, diseñada para ataques devastadores y defensa sólida.',
+      gif: 'img/nave3.gif'
+    }
+  ];
+
+  let indiceNave = 0;
+
+  function actualizarNave() {
+    $('#gif-nave').attr('src', naves[indiceNave].gif);
+    $('#gif-nave').attr('alt', 'GIF nave ' + naves[indiceNave].nombre);
+    $('#nombre-nave').text(naves[indiceNave].nombre);
+    $('#descripcion-nave').text(naves[indiceNave].descripcion);
+  }
+
+  // Eventos para cambiar de nave
+  $('#flecha-derecha').on('click', function() {
+    indiceNave = (indiceNave + 1) % naves.length;
+    actualizarNave();
+  });
+
+  $('#flecha-izquierda').on('click', function() {
+    indiceNave = (indiceNave - 1 + naves.length) % naves.length;
+    actualizarNave();
+  });
+
+  // Inicializar con la primera nave
+  actualizarNave();
 });
