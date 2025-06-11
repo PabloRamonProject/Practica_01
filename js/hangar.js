92a1c1ccb40050da72f6a27919f35baf7e96a1ae
PabloRamonProject <pablo.r.bravo.kos@gmail.com>
2025-06-11 21:33:07 +0200

    Seleccion de faccion

---
+++ js/hangar.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  // Cambiar color de fondo según facción seleccionada
+  $('.btn-faccion').on('click', function() {
+    const faccion = $(this).data('faccion');
+
+    // Resetear background
+    $('body').css('background-color', '');
+
+    if (faccion === 'VRU') {
+      $('body').css('background-color', '#013300');  // verde muy oscuro
+    } else if (faccion === 'MMO') {
+      $('body').css('background-color', '#4b0000');  // rojo muy oscuro
+    } else if (faccion === 'EIC') {
+      $('body').css('background-color', '#001933');  // azul muy oscuro
+    }
+  });
+
+  // Mostrar imagen al hacer hover sobre botón de facción
+  const faccionImages = {
+    'MMO': 'img/facciones/mmo.png',
+    'EIC': 'img/facciones/eic.png',
+    'VRU': 'img/facciones/vru.png'
+  };
+
+  $('.btn-faccion').on('mouseenter', function() {
+    const faccion = $(this).data('faccion');
+    const imgSrc = faccionImages[faccion] || '';
+
+    if (imgSrc) {
+      $('#preview-image').attr('src', imgSrc).fadeIn(200);
+    }
+  });
+
+  $('.btn-faccion').on('mouseleave', function() {
+    $('#preview-image').fadeOut(200);
+  });
+});
